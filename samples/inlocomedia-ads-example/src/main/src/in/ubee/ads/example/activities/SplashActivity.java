62bf9d337218b26cdc6e7916ebdf4b3598bc18d5
Leocadio Tiné <leocadiotine@gmail.com>
2015-02-27 15:28:49 -0300

    Removes the Timer from SplashActivity. It was causing a memory leak.

--- samples/inlocomedia-ads-example/src/main/src/in/ubee/ads/example/activities/SplashActivity.java
+++ samples/inlocomedia-ads-example/src/main/src/in/ubee/ads/example/activities/SplashActivity.java
@@ -1,54 +1,47 @@
 package in.ubee.ads.example.activities;
 
-import in.ubee.ads.example.R;
-import in.ubee.ads.example.application.InLocoMedia;
-
-import java.util.Timer;
-import java.util.TimerTask;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
-import android.view.View.OnClickListener;
+
+import in.ubee.ads.example.R;
+import in.ubee.ads.example.application.InLocoMedia;
 
 public class SplashActivity extends Activity {
 
     private static final long TIMEOUT = 3000;
-
-    private Timer timer;
 
     @Override
     public void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         InLocoMedia.setup(this);
-        
+
         this.setContentView(R.layout.activity_splash);
-
         View view = findViewById(R.id.splash_screen_click_view_id);
-
-        view.setOnClickListener(new OnClickListener() {
+        view.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
-                timer.cancel();
-                startActivity(new Intent(SplashActivity.this, MainActivity.class));
-                finish();
+                goToMainActivity();
             }
         });
 
-        TimerTask task = new TimerTask() {
-
+        new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                SplashActivity.this.startActivity(new Intent(SplashActivity.this, MainActivity.class));
-                SplashActivity.this.finish();
-                finish();
+
+                if (!isFinishing()) {
+                    goToMainActivity();
+                }
             }
-        };
-        this.timer = new Timer();
-        this.timer.schedule(task, TIMEOUT);
+        }, TIMEOUT);
     }
 
+    private void goToMainActivity() {
+        startActivity(new Intent(SplashActivity.this, MainActivity.class));
+        finish();
+    }
 }
