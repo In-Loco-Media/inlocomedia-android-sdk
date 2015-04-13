84c47934fcbff893844bee011cdd0e0f5ed47103
Gabriel Falcone <falconegabriel@gmail.com>
2015-04-13 11:22:07 -0300

    Append ao ultimo commit

--- samples/inlocomedia-ads-sample/src/main/src/in/ubee/ads/example/application/Settings.java
+++ samples/inlocomedia-ads-sample/src/main/src/in/ubee/ads/example/application/Settings.java
@@ -6,7 +6,7 @@ public final class Settings {
 
     public static boolean DEBUG = true;
 
-    public static final String INLOCOMEDIA_KEY = "1e14a5243a6a8c2d2a0aec8ba777ee3afe8e70f51c5d6f2f7a287024cd3a9b22";
+    public static final String INLOCOMEDIA_KEY = "46cdb49a530d3976c31a4729fc6cea7ea1bb650f56379d7c83a1e6047a7acbca";
     public static final String INLOCOMEDIA_SECRET = "056c59cd2984b5cf0f313ff5f1d88f5b7f7eabc5e577bf96f99db4184fd7aa92";
 
 }
