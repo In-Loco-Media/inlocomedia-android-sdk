0de79f156f912208da380c502bd79146bb399d34
Larissa Navarro <larissa.navarro@inlocomedia.com>
2016-01-27 14:55:13 -0300

    Native ad use cases

--- samples/inlocomedia-ads-sample/src/main/java/com/inlocomedia/ads/sample/activities/NativeAdActivity.java
+++ samples/inlocomedia-ads-sample/src/main/java/com/inlocomedia/ads/sample/activities/NativeAdActivity.java
@@ -1,55 +1,102 @@
 package com.inlocomedia.ads.sample.activities;
 
-import com.inlocomedia.ads.sample.R;
-import com.inlocomedia.ads.sample.activities.util.BaseActivity;
-import com.inlocomedia.ads.sample.activities.util.ListItem;
-import in.ubee.api.ads.AdType;
-import in.ubee.api.ads.AdvertisementAdapter;
+import android.app.Activity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 
-public class NativeAdActivity extends BaseActivity {
+import com.inlocomedia.ads.sample.R;
+import com.inlocomedia.ads.sample.activities.util.ListItem;
 
-    private final String[] listValues = new String[] { "Lorem Ipsum",
-                                                       "Lorem Ipsum",
-                                                       "Lorem Ipsum",
-                                                       "Lorem Ipsum",
-                                                       "Lorem Ipsum",
-                                                       "Lorem Ipsum",
-                                                       "Lorem Ipsum",
-                                                       "Lorem Ipsum",
-                                                       "Lorem Ipsum",
-                                                       "Lorem Ipsum",
-                                                       "Lorem Ipsum",
-                                                       "Lorem Ipsum" };
+import in.ubee.api.ads.AdError;
+import in.ubee.api.ads.AdType;
+import in.ubee.api.ads.AdView;
+import in.ubee.api.ads.AdViewListener;
+
+/**
+ * Created by: larissa
+ * Date: 1/27/16
+ * Time: 2:25 PM
+ */
+public class NativeAdActivity extends Activity {
+    private AdView mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.list_activity_layout);
+        this.setContentView(R.layout.activity_native_ad);
 
         // The selected item list from the previous screen that contains the AdType that will be loaded
-        final ListView listView = (ListView) findViewById(android.R.id.list);
-
         ListItem item = (ListItem) getIntent().getSerializableExtra(MainActivity.EXTRA_ITEM);
 
-        if (item.getAdType() == AdType.NATIVE_LARGE) {
-            ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.list_item_large, android.R.id.text1, listValues);
-            AdvertisementAdapter adAdapter = new AdvertisementAdapter(this, adapter, R.layout.ad_large_list_item, 6);
-            listView.setAdapter(adAdapter);
+        FrameLayout parent = (FrameLayout) findViewById(R.id.native_ad_container);
 
-        } else if (item.getAdType() == AdType.NATIVE_OFFER) {
-            final ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.small_list_item, android.R.id.text1, listValues);
-            AdvertisementAdapter adAdapter = new AdvertisementAdapter(this, adapter, R.layout.ad_coupon_list_item, 6);
-            listView.setAdapter(adAdapter);
+        // Inflate correct layout
+        LayoutInflater inflater = LayoutInflater.from(this);
+
+        if (item.getAdType() == AdType.NATIVE_LARGE) {
+            mAdView = (AdView) inflater.inflate(R.layout.native_ad_large, parent, false);
 
         } else if (item.getAdType() == AdType.NATIVE_SMALL) {
-            final ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.small_list_item, android.R.id.text1, listValues);
-            final AdvertisementAdapter adAdapter = new AdvertisementAdapter(this, adapter, R.layout.ad_small_list_item, 8);
-            listView.setAdapter(adAdapter);
+            mAdView = (AdView) inflater.inflate(R.layout.native_ad_small, parent, false);
         }
 
+        mAdView.setVisibility(View.INVISIBLE);
+
+        // Set the AdType
+        mAdView.setType(item.getAdType());
+
+        // Register a listener to receive notifications for the AdView events
+        mAdView.setAdListener(new AdViewListener() {
+            public void onAdViewReady(AdView adView) {
+                /**
+                 * The AdView is ready to be shown
+                 */
+                mAdView.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onAdError(AdView adView, AdError adError) {
+                /**
+                 * The AdView load has failed. Check the AdErrors to discover the reason
+                 */
+                onError(adError.toString());
+            }
+        });
+
+        mAdView.loadAd();
+
+        // Display the ad
+        parent.addView(mAdView);
+    }
+
+    private void onError(String error) {
+        // Show the DisplayAdActivity with the AdError
+        TextView view = (TextView) findViewById(R.id.native_description);
+        view.setText("Error: " + error);
+        view.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Notify the Activity's lifecycle
+        mAdView.resume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // Notify the Activity's lifecycle
+        mAdView.pause(isFinishing());
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // Notify the Activity's lifecycle
+        mAdView.destroy();
     }
 }
